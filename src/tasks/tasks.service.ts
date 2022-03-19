21ec2a1441f4c123727338654e9f115f893af116
pscherbinov <panadivel.v2@gmail.com>
2022-03-19 19:01:24 +0300

    add task by id validations

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -34,10 +34,17 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((t: Task) => t.id === id);
+    const found = this.tasks.find((t: Task) => t.id === id);
+
+    if (!found) {
+      throw new NotFoundException(`Task with id = ${id} not found`);
+    }
+
+    return found;
   }
 
   deleteTaskById(id: string): void {
+    const found = this.getTaskById(id);
     this.tasks = this.tasks.filter((t: Task) => t.id !== id);
   }
 
