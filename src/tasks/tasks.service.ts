881ccf18998636a1fa1c14d36b04ffc5fd67e84a
pscherbinov <panadivel.v2@gmail.com>
2022-03-23 20:46:49 +0300

    remove old methods

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -17,26 +17,6 @@ export class TasksService {
     return this.tasksRepository.getTasks(filterDto);
   }
 
-  // getAllTasks(): Task[] {
-  //   return this.tasks;
-  // }
-  // getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-  //   const { status, search } = filterDto;
-  //   let tasks = this.getAllTasks();
-  //   if (status) {
-  //     tasks = tasks.filter((t) => t.status === status);
-  //   }
-  //   if (search) {
-  //     tasks = tasks.filter((t) => {
-  //       if (t.title.includes(search) || t.description.includes(search)) {
-  //         return true;
-  //       }
-  //       return false;
-  //     });
-  //   }
-  //   return tasks;
-  // }
-
   async getTaskById(id: string): Promise<Task> {
     const found = await this.tasksRepository.findOne(id);
 
@@ -46,11 +26,6 @@ export class TasksService {
 
     return found;
   }
-
-  // deleteTaskById(id: string): void {
-  //   const found = this.getTaskById(id);
-  //   this.tasks = this.tasks.filter((t: Task) => t.id !== id);
-  // }
 
   async deleteTaskById(id: string): Promise<void> {
     const result = await this.tasksRepository.delete(id);
