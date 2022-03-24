5c9c935af5a5337b583d09489e9d9415beac9587
pscherbinov <panadivel.v2@gmail.com>
2022-03-24 23:27:20 +0300

    add unique to username column, error handler

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,3 +1,7 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
@@ -8,6 +12,14 @@ export class UsersRepository extends Repository<User> {
     const { username, password } = authCredentialsDto;
 
     const user = this.create({ username, password });
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (err) {
+      if (err.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
