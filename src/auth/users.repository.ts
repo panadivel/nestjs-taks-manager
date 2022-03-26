b664af51807863e0cda74efe56b81ce8a496ef1b
pscherbinov <panadivel.v2@gmail.com>
2022-03-26 23:55:37 +0300

    add bcrypt

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -5,13 +5,17 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto) {
     const { username, password } = authCredentialsDto;
 
-    const user = this.create({ username, password });
+    const salt = await bcrypt.getSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
     try {
       await this.save(user);
     } catch (err) {
