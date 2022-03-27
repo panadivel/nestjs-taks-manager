695ea572fa920af15b26fa1b946f736e3d6fdb7a
pscherbinov <panadivel.v2@gmail.com>
2022-03-27 18:07:05 +0300

    enable logger

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: false,
+    logger: ['error', 'warn'],
   });
   app.useGlobalPipes(new ValidationPipe());
 
